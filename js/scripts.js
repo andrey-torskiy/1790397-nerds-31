fe984fe13158f115178cd96b3bc20202450412ef
Andrey <molkovandrei@yandex.ru>
2021-07-02 22:04:08 +0300

    Update scripts.js
    camelCase + оживление слайдера

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,99 @@
 
 // Open modal window
-let modal_send_mail = document.querySelector(".modal-send-mail");
-let modal_form = modal_send_mail.querySelector(".modal-form")
-let contact_button = document.querySelector(".contacts-button");
+let modalSendMail = document.querySelector(".modal-send-mail");
+let modalForm = modalSendMail.querySelector(".modal-form")
+let contactButton = document.querySelector(".contacts-button");
 
-let close_modal_button = document.querySelector(".close-modal-button");
-let name_input = modal_send_mail.querySelector(".sender-username");
-let email_input = modal_send_mail.querySelector(".sender-email");
-let text_input = modal_send_mail.querySelector(".sender-text");
+let closeModalButton = document.querySelector(".close-modal-button");
+let nameInput = modalSendMail.querySelector(".sender-username");
+let emailInput = modalSendMail.querySelector(".sender-email");
+let textInput = modalSendMail.querySelector(".sender-text");
 
 let isStorageSupport = true;
-let storage_name = "";
-let storage_email = "";
+let storageName = "";
+let storageEmail = "";
 
 try {
-    storage_name = localStorage.getItem("name");
-    storage_email = localStorage.getItem("email");
+    storageName = localStorage.getItem("name");
+    storageEmail = localStorage.getItem("email");
 } catch (er) {
     isStorageSupport = false;
 }
 
-contact_button.addEventListener("click", function(evt) {
+contactButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modal_send_mail.classList.add("modal-send-active");
-    name_input.focus();
-    if (storage_name) {
-        name_input.value = storage_name;
-        email_input.focus();
+    modalSendMail.classList.add("modal-send-active");
+    nameInput.focus();
+    if (storageName) {
+        nameInput.value = storageName;
+        emailInput.focus();
     }
-    if (storage_email) {
-        email_input.value = storage_email;
-        text_input.focus();
+    if (storageEmail) {
+        emailInput.value = storageEmail;
+        textInput.focus();
     }
 });
 
-name_input.addEventListener("input", function(evt) {
-    name_input.classList.remove("no-data");
+nameInput.addEventListener("input", function(evt) {
+    nameInput.classList.remove("no-data");
 });
 
-email_input.addEventListener("input", function(evt) {
-    email_input.classList.remove("no-data");
+emailInput.addEventListener("input", function(evt) {
+    emailInput.classList.remove("no-data");
 });
 
-text_input.addEventListener("input", function(evt) {
-    text_input.classList.remove("no-data");
+textInput.addEventListener("input", function(evt) {
+    textInput.classList.remove("no-data");
 });
 
 // Close modal window
-close_modal_button.onclick = function() {
-    name_input.value = "";
-    email_input.value = "";
-    text_input.value = "";
-    modal_send_mail.classList.remove("modal-send-active");
-    name_input.classList.remove("no-data");
-    email_input.classList.remove("no-data");
-    text_input.classList.remove("no-data");
-    modal_send_mail.classList.remove("modal-error");
+closeModalButton.onclick = function() {
+    nameInput.value = "";
+    emailInput.value = "";
+    textInput.value = "";
+    modalSendMail.classList.remove("modal-send-active");
+    nameInput.classList.remove("no-data");
+    emailInput.classList.remove("no-data");
+    textInput.classList.remove("no-data");
+    modalSendMail.classList.remove("modal-error");
 }
 
-modal_form.addEventListener("submit", function(evt) {
-    if (name_input.value == "") {
+modalForm.addEventListener("submit", function(evt) {
+    if (nameInput.value == "") {
         evt.preventDefault();
-        name_input.classList.add("no-data");
-        modal_send_mail.classList.add("modal-error");
+        nameInput.classList.add("no-data");
+        modalSendMail.classList.add("modal-error");
     }
-    if (email_input.value == "") {
+    if (emailInput.value == "") {
         evt.preventDefault();
-        email_input.classList.add("no-data");
-        modal_send_mail.classList.add("modal-error");
+        emailInput.classList.add("no-data");
+        modalSendMail.classList.add("modal-error");
     }
-    if (text_input.value == "") {
+    if (textInput.value == "") {
         evt.preventDefault();
-        text_input.classList.add("no-data");
-        modal_send_mail.classList.add("modal-error");
+        textInput.classList.add("no-data");
+        modalSendMail.classList.add("modal-error");
     }
 
-    if (name_input.value && email_input.value && text_input.value) {
-        localStorage.setItem("name", name_input.value);
-        localStorage.setItem("email", email_input.value)
+    if (nameInput.value && emailInput.value && textInput.value) {
+        localStorage.setItem("name", nameInput.value);
+        localStorage.setItem("email", emailInput.value)
     }
 
 });
 
 window.addEventListener("keydown", function(evt) {
     if (evt.key === "Esc" || evt.key === "Escape") {
-        if (modal_send_mail.classList.contains("modal-send-active")) {
+        if (modalSendMail.classList.contains("modal-send-active")) {
             evt.preventDefault();          
-            name_input.value = "";
-            email_input.value = "";
-            text_input.value = "";
-            modal_send_mail.classList.remove("modal-send-active");
-            name_input.classList.remove("no-data");
-            email_input.classList.remove("no-data");
-            text_input.classList.remove("no-data");
-            modal_send_mail.classList.remove("modal-error");
+            nameInput.value = "";
+            emailInput.value = "";
+            textInput.value = "";
+            modalSendMail.classList.remove("modal-send-active");
+            nameInput.classList.remove("no-data");
+            emailInput.classList.remove("no-data");
+            textInput.classList.remove("no-data");
+            modalSendMail.classList.remove("modal-error");
         }
     }
 });
@@ -102,62 +102,62 @@ window.addEventListener("keydown", function(evt) {
 
 // slides
 
-let slides_list = document.querySelectorAll(".slider-item");
-let sliders_buttons = document.querySelectorAll(".slider-navigation-button");
-let slide_id = 0;
+let slidesList = document.querySelectorAll(".slider-item");
+let slidersButtons = document.querySelectorAll(".slider-navigation-button");
+let slideId = 0;
 
-for (let i=0; i < sliders_buttons.length; i++) {
-    sliders_buttons[i].addEventListener("click", function(evt) {
-        for (let sl_btn of sliders_buttons) {
-            sl_btn.classList.remove("slider-nav-button-current");
+for (let i=0; i < slidersButtons.length; i++) {
+    slidersButtons[i].addEventListener("click", function(evt) {
+        for (let slBtn of slidersButtons) {
+            slBtn.classList.remove("slider-nav-button-current");
         }
-        sliders_buttons[i].classList.add("slider-nav-button-current");
-        show_slide(i);
-        slides_change();
+        slidersButtons[i].classList.add("slider-nav-button-current");
+        showSlide(i);
+        slidesChange();
     });
 };
 
 
-function show_slide(i_slide) {
-    slide_id = i_slide;
-    if (i_slide >= slides_list.length) {
-      slide_id = 0;
+function showSlide(iSlide) {
+    slideId = iSlide;
+    if (iSlide >= slidesList.length) {
+      slideId = 0;
     }
 
-    if (i_slide < 0) {
-        slide_id = slides_list.length - 1;
+    if (iSlide < 0) {
+        slideId = slidesList.length - 1;
     }
  
-    for (let slide of slides_list) {
+    for (let slide of slidesList) {
         slide.classList.remove("slide-current");
     }
-    slides_list[slide_id].classList.add("slide-current");    
+    slidesList[slideId].classList.add("slide-current");    
   }
 
-function set_button(i_button) {
-    button_id = i_button;
-    if (i_button >= sliders_buttons.length) {
-        button_id = 0;
+function setButton(iButton) {
+    buttonId = iButton;
+    if (iButton >= slidersButtons.length) {
+        buttonId = 0;
     }
 
-    if (i_button < 0) {
-        button_id = sliders_buttons.length - 1;
+    if (iButton < 0) {
+        buttonId = slidersButtons.length - 1;
     }
 
-    for (let slider_button of sliders_buttons) {
-        slider_button.classList.remove("slider-nav-button-current");
+    for (let sliderButton of slidersButtons) {
+        sliderButton.classList.remove("slider-nav-button-current");
     }
-    sliders_buttons[button_id].classList.add("slider-nav-button-current");
+    slidersButtons[buttonId].classList.add("slider-nav-button-current");
 }
 
 let timer = 0;
-function slides_change() {
+function slidesChange() {
     clearInterval(timer);
     timer = setInterval(function() {
-        slide_id++;
-        show_slide(slide_id);
-        set_button(slide_id);
+        slideId++;
+        showSlide(slideId);
+        setButton(slideId);
     }, 5000)
 }
 
-slides_change();
+slidesChange();
